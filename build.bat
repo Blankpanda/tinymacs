77b37d3159164672465d711d7c34a83b8ea60610
Blankpanda <Blankpanda8@gmail.com>
2016-06-16 21:05:42 -0400

    file structure

--- build.bat
+++
@@ -1,6 +0,0 @@
-@echo on
-
-SET deps=user32.lib Gdi32.lib
-SET entry=win32_main.cpp
-
-cl %entry% /link /subsystem:windows %deps%
--- clean.bat
+++
@@ -1,5 +0,0 @@
-:: hack 
-@echo off
-
-del %~dp0\win32_main.obj
-del %~dp0\win32_main.exe
